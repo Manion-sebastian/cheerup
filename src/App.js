6b8c5372251c9f814b6d2595e115641ab162c968
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-11-02 13:11:10 -0700

    fixed uppercase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { useState  } from 'react';
+import { useState, useEffect  } from 'react';
 import './App.css';
 
 const Peeps = ['MIKA', 'ELI', 'DERYA', 'MARCIE', 'DANNY', 'HENRY', 'EMMA', 'LEILA', 'SEBASTIAN', 'MEL']
 const sortedPeeps = Peeps.sort()
 
-function App() {
 
+function App() {
+  
   const peepMap = sortedPeeps.map(peep => {
     return <button onClick={() => setPerson(peep)}>{peep}</button>
   })
-
+  
   const [person, setPerson] = useState('')
+  // useEffect(() => {
+  //   setPerson(person.toUpperCase())
+  // }, [person])
   const handlePerson = e => {
-    setPerson(e.target.value)
+    setPerson(e.target.value.toUpperCase())
   }
   return (
     <div className="App">
